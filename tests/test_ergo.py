769258dc4bd18307d4d66fb34c9134bdcc74672c
Ben Rachbach <brachbach@gmail.com>
2020-03-25 20:43:37 -0700

    Add seaborn and test of show_submission

--- tests/test_ergo.py
+++ tests/test_ergo.py
@@ -4,20 +4,23 @@ test_uname = "oughttest"
 test_pwd = "6vCo39Mz^rrb"
 test_user_id = 112420
 
+mock_sample = [1, 2, 3]
+
 
 def test_version():
     assert ergo.__version__ == '0.2.0'
 
 
-def test_login():
+class TestMetaculus:
     metaculus = ergo.Metaculus(test_uname, test_pwd)
-    assert metaculus.user_id == test_user_id
-
-
-def test_submission():
-    metaculus = ergo.Metaculus(test_uname, test_pwd)
-
-    # need to replace this with a different question once this one resolves on Mar 27
     euro_question = metaculus.get_question(3706)
-    r = euro_question.submit(0.534894790856232, 0.02)
-    assert r.status_code == 202
+
+    def test_login(self):
+        assert self.metaculus.user_id == test_user_id
+
+    def test_submission(self):
+        r = self.euro_question.submit(0.534894790856232, 0.02)
+        assert r.status_code == 202
+
+    def test_show_submission(self):
+        self.euro_question.show_submission(mock_sample)
