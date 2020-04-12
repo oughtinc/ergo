e5f5858c866ef487b30ecb8441544bd89391af1f
Ben Rachbach <brachbach@gmail.com>
2020-04-11 21:04:18 -0700

    Figure out how to get true scale values from sub dist values

--- investigate_metaculus_api/log_scale_predictions.py
+++ investigate_metaculus_api/log_scale_predictions.py
@@ -1,5 +1,6 @@
 # https: // www.metaculus.com/questions/4111/for-testing-log-question-open-both-ends-1000-to-2000/
 
+from scipy import stats
 true_loc = 1300
 true_25th = 1000
 true_75th = 1700
@@ -99,14 +100,14 @@ def get_true_loc(submission_loc, scale):
     return scaled
 
 
-print(get_true_loc(submission_loc, scale))
+# print(get_true_loc(submission_loc, scale))
 
 prediction_request_loc_1900 = {"prediction": {"kind": "multi", "d": [
     {"kind": "logistic", "x0": 0.9278, "s": 0.3727, "w": 1, "low": 0.061, "high": 0.553}]}, "void": False}
 
 submission_median_1900 = prediction_request_loc_1900["prediction"]["d"][0]
 
-print(get_true_loc(submission_median_1900["x0"], scale))
+# print(get_true_loc(submission_median_1900["x0"], scale))
 
 
 def get_true_scale(submission_scale, scale):
@@ -116,3 +117,24 @@ def get_true_scale(submission_scale, scale):
     scaled = expd * width
 
     return scaled
+
+
+submission_dist = stats.logistic(submission_loc, submission_scale)
+
+
+def get_true_value_from_sub_dist_value(sub_dist_value, scale):
+    expd = scale["deriv_ratio"] ** sub_dist_value
+
+    width = scale["max"] - scale["min"]
+    scaled = expd * width
+
+    return scaled
+
+
+print(submission_dist.ppf(0.5))
+
+print(get_true_value_from_sub_dist_value(submission_dist.ppf(0.5), scale))
+
+print(get_true_value_from_sub_dist_value(submission_dist.ppf(0.25), scale))
+
+print(get_true_value_from_sub_dist_value(submission_dist.ppf(0.75), scale))
