ae47c19b7a7f303887a80e20a4553b44131cdc1f
Ben Rachbach <brachbach@gmail.com>
2020-04-13 20:21:02 -0700

    Try to show prediction but it's on the wrong plot

--- ergo/metaculus_question.py
+++ ergo/metaculus_question.py
@@ -293,9 +293,6 @@ class ContinuousQuestion(MetaculusQuestion):
         latest_prediction = self.my_predictions["predictions"][-1]["d"]
         return self.get_submission_from_json(latest_prediction)
 
-    def show_community_prediction(self):
-        raise NotImplementedError("This should be implemented by a subclass")
-
     # TODO: show vs. Metaculus and vs. resolution if available
     def show_performance(self):
         mixture_params = self.get_latest_normalized_prediction()
@@ -339,18 +336,6 @@ class LinearQuestion(ContinuousQuestion):
         submission = self.get_submission_from_samples(samples)
 
         self.show_prediction(submission, samples)
-
-    def show_community_prediction(self, only_show_this=True):
-        if only_show_this:
-            pyplot.figure()
-            pyplot.title(f"{self} community prediction")
-
-        community_samples = [self.sample_community() for _ in range(0, 1000)]
-
-        ax = seaborn.distplot(
-            community_samples, label="Community")
-
-        return ax
 
 
 class LogQuestion(ContinuousQuestion):
@@ -400,117 +385,3 @@ class LogQuestion(ContinuousQuestion):
         submission = self.get_submission_from_samples(samples)
 
         self.show_prediction(submission, samples)
-
-    def show_community_prediction(self, only_show_this=True):
-        if only_show_this:
-            pyplot.figure()
-            pyplot.title(f"{self} community prediction")
-
-        community_samples = [self.sample_community() for _ in range(0, 1000)]
-        pyplot.xscale("log")  # type: ignore
-
-        ax = seaborn.distplot(
-            community_samples, label="Community")
-
-        return ax
-
-
-class Metaculus:
-    player_status_to_api_wording = {
-        "predicted": "guessed_by",
-        "not-predicted": "not_guessed_by",
-        "author": "author",
-        "interested": "upvoted_by",
-    }
-
-    def __init__(self, username, password, api_domain="www"):
-        self.user_id = None
-        self.api_url = f"https://{api_domain}.metaculus.com/api2"
-        self.s = requests.Session()
-        self.login(username, password)
-
-    def login(self, username, password):
-        loginURL = f"{self.api_url}/accounts/login/"
-        r = self.s.post(loginURL,
-                        headers={"Content-Type": "application/json", },
-                        data=json.dumps({"username": username, "password": password}))
-
-        self.user_id = r.json()['user_id']
-
-    def post(self, url: str, data: Dict):
-        r = self.s.post(
-            url,
-            headers={
-                "Content-Type": "application/json",
-                "Referer": self.api_url,
-                "X-CSRFToken": self.s.cookies.get_dict()["csrftoken"]
-            },
-            data=json.dumps(data)
-        )
-        try:
-            r.raise_for_status()
-
-        except requests.exceptions.HTTPError as e:
-            e.args = (str(
-                e.args), f"request body: {e.request.body}", f"response json: {e.response.json()}")
-            raise
-
-        return r
-
-    def make_question_from_data(self, data=Dict, name=None) -> MetaculusQuestion:
-        if(data["possibilities"]["type"] == "binary"):
-            return BinaryQuestion(data["id"], self, data, name)
-        if(data["possibilities"]["type"] == "continuous"):
-            if(data["possibilities"]["scale"]["deriv_ratio"] != 1):
-                return LogQuestion(data["id"], self, data, name)
-            return LinearQuestion(data["id"], self, data, name)
-        raise NotImplementedError(
-            "We couldn't determine whether this question was binary, continuous, or something else")
-
-    def get_question(self, id: int, name=None) -> MetaculusQuestion:
-        r = self.s.get(f"{self.api_url}/questions/{id}")
-        data = r.json()
-        return self.make_question_from_data(data, name)
-
-    def get_questions_json(self,
-                           question_status: Literal["all", "upcoming", "open", "closed", "resolved", "discussion"] = "all",
-                           player_status: Literal["any", "predicted",
-                                                  "not-predicted", "author", "interested", "private"] = "any",
-                           # 20 results per page
-                           pages: int = 1,
-                           ) -> List[Dict]:
-        query_params = [f"status={question_status}", "order_by=-publish_time"]
-        if player_status != "any":
-            if player_status == "private":
-                query_params.append("access=private")
-            else:
-                query_params.append(
-                    f"{self.player_status_to_api_wording[player_status]}={self.user_id}")
-
-        query_string = "&".join(query_params)
-
-        def get_questions_for_pages(query_string: str, max_pages: int = 1, current_page: int = 1, results=[]) -> List[Dict]:
-            if current_page > max_pages:
-                return results
-
-            r = self.s.get(
-                f"{self.api_url}/questions/?{query_string}&page={current_page}")
-
-            if r.json() == {"detail": "Invalid page."}:
-                return results
-
-            r.raise_for_status()
-
-            return get_questions_for_pages(query_string, max_pages, current_page + 1, results + r.json()["results"])
-
-        return get_questions_for_pages(query_string, pages)
-
-    def make_questions_df(self, questions_json):
-        questions_df = pd.DataFrame(questions_json)
-        for col in ["created_time", "publish_time", "close_time", "resolve_time"]:
-            questions_df[col] = questions_df[col].apply(pendulum.parse)
-
-        questions_df["i_created"] = questions_df["author"] == self.user_id
-        questions_df["i_predicted"] = questions_df["my_predictions"].apply(
-            lambda x: x is not None)
-        return questions_df
