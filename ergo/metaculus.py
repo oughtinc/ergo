b8dd24a445f06c8dab65c7c18409aba93a083776
Zachary Miller <zachary.john.miller@gmail.com>
2020-04-18 13:40:33 -0600

    Remove superfluous method

--- ergo/metaculus.py
+++ ergo/metaculus.py
@@ -621,9 +621,6 @@ class Metaculus:
 
         return get_questions_for_pages(query_string, pages)
 
-    def get_questions_json_in_cat(self, cat: str) -> List[Dict]:
-        return self.get_questions_json(cat=cat)
-
     def make_questions_df(self, questions_json):
         questions_df = pd.DataFrame(questions_json)
         for col in ["created_time", "publish_time", "close_time", "resolve_time"]:
