5a1bb3cb0a3afedf4ced9dcb086880d509739e8b
Zachary Miller <zachary.john.miller@gmail.com>
2020-04-30 12:27:59 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="ergo",
+    version="1.0",
+    description="description",
+    author="author",
+    author_email="author_email@author_email.net",
+    url="https://www.url.org/",
+    packages=["ergo"],
+)
