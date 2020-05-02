71264c8289ce45da61cb92649e50a1254bacb1d0
D J Hawthorne <djthorne@users.noreply.github.com>
2020-05-02 22:11:57 +0200

    rename directories to src and build

---
+++ src/strip_output.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+from pathlib import Path
+
+path = Path('..').cwd() / 'src'
+os.chdir(path)
+
+for file in path.glob("*.ipynb"):
+    subprocess.run(f"jupytext --to md {Path(file).name}", shell=True, check=True, stdout=subprocess.PIPE)
+    res = subprocess.run(f"jupytext --to notebook {Path(file).stem}.md", shell=True, check=True, universal_newlines=True, stdout=subprocess.PIPE)
+    print(res.stdout.split("\n")[1])
+    subprocess.run(f"rm {Path(file).stem}.md", shell=True, check=True, stdout=subprocess.PIPE)
