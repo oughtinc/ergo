39cd277b89e7d55dc1c423d256b05c0a072ad449
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2020-05-02 20:21:08 -0700

    Make scrubbing more robust

--- scripts/scrub_notebooks.py
+++ scripts/scrub_notebooks.py
@@ -1,25 +1,35 @@
 import os
+import argparse
 from pathlib import Path
 import subprocess
 
-path = Path("..").cwd() / "src"
-os.chdir(path)
+def scrub(notebooks_path, scrubbed_path):
+    for notebook_file in notebooks_path.glob("*.ipynb"):
+        scrubbed_file = Path(scrubbed_path) / notebook_file.name
+        subprocess.run(
+            f"jupytext --output '{scrubbed_file}.md' --to md '{notebook_file}'",
+            shell=True,
+            check=True,
+            stdout=subprocess.PIPE,
+        )
+        res = subprocess.run(
+            f"jupytext --output '{scrubbed_file}' --to notebook '{scrubbed_file}.md'",
+            shell=True,
+            check=True,
+            universal_newlines=True,
+            stdout=subprocess.PIPE,
+        )
+        print(res.stdout.split("\n")[1])
+        subprocess.run(
+            f"rm '{scrubbed_file}.md'", shell=True, check=True, stdout=subprocess.PIPE
+        )
 
-for file in path.glob("*.ipynb"):
-    subprocess.run(
-        f"jupytext --to md {Path(file).name}",
-        shell=True,
-        check=True,
-        stdout=subprocess.PIPE,
-    )
-    res = subprocess.run(
-        f"jupytext --to notebook {Path(file).stem}.md",
-        shell=True,
-        check=True,
-        universal_newlines=True,
-        stdout=subprocess.PIPE,
-    )
-    print(res.stdout.split("\n")[1])
-    subprocess.run(
-        f"rm {Path(file).stem}.md", shell=True, check=True, stdout=subprocess.PIPE
-    )
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("notebooks_path", type=Path)
+    parser.add_argument("scrubbed_path", type=Path)
+    p = parser.parse_args()
+    assert os.path.exists(p.notebooks_path)
+    assert os.path.exists(p.scrubbed_path)
+    scrub(p.notebooks_path, p.scrubbed_path)
+    
