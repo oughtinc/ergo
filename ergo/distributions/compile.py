5342324c2b41778786849ebd6e877ea716d0f5b7
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2020-05-27 18:29:43 -0700

    Compile loss function computations with global jit state

---
+++ ergo/distributions/compile.py
@@ -0,0 +1,38 @@
+from typing import Iterator, List
+
+import jax.numpy as np
+
+from ergo.distributions.conditions import (
+    Condition,
+    CrossEntropyCondition,
+    IntervalCondition,
+    MaxEntropyCondition,
+    SmoothnessCondition,
+)
+from ergo.distributions.histogram import HistogramDist
+
+
+def single_interval_condition_cases():
+    for lower_bound in [None, 0.3]:
+        for upper_bound in [None, 0.9]:
+            yield IntervalCondition(p=0.5, min=lower_bound, max=upper_bound)
+
+
+def compile_histogram_loss_functions(num_bins: int = 201):
+    print("Compiling loss functions...")
+    dist = HistogramDist(np.array([-float(num_bins)] * num_bins))
+    conditions = list(single_interval_condition_cases()) + [
+        CrossEntropyCondition(dist),
+        MaxEntropyCondition(),
+        SmoothnessCondition(weight=10.0),
+    ]
+    for condition in conditions:
+        dist = HistogramDist.from_conditions(
+            [condition], scale_min=0, scale_max=1, num_bins=num_bins
+        )
+        condition.describe_fit(dist)
+    print("Compilation done!")
+
+
+def compile():
+    compile_histogram_loss_functions()
--- ergo/distributions/histogram.py
+++ ergo/distributions/histogram.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import partial
 from typing import List
 
 from jax import grad, jit, nn
@@ -13,33 +14,45 @@ from . import conditions, distribution
 class HistogramDist(distribution.Distribution):
     logps: np.DeviceArray
 
-    def __init__(self, logps, scale_min=0, scale_max=1, traceable=False):
-        init_numpy = np if traceable else onp
-        self.logps = logps
-        self.ps = np.exp(logps)
-        self.cum_ps = np.array(init_numpy.cumsum(self.ps))
-        self.bins = np.linspace(scale_min, scale_max, logps.size + 1)
-        self.size = logps.size
-        self.scale_min = scale_min
-        self.scale_max = scale_max
+    def __init__(
+        self, logps=None, scale_min=0, scale_max=1, traceable=False, direct_init=None
+    ):
+        if direct_init:
+            self.logps = direct_init["logps"]
+            self.ps = direct_init["ps"]
+            self.cum_ps = direct_init["cum_ps"]
+            self.bins = direct_init["bins"]
+            self.size = direct_init["size"]
+            self.scale_min = direct_init["scale_min"]
+            self.scale_max = direct_init["scale_max"]
+        else:
+            init_numpy = np if traceable else onp
+            self.logps = logps
+            self.ps = np.exp(logps)
+            self.cum_ps = np.array(init_numpy.cumsum(self.ps))
+            self.bins = np.linspace(scale_min, scale_max, logps.size + 1)
+            self.size = logps.size
+            self.scale_min = scale_min
+            self.scale_max = scale_max
 
     def entropy(self):
         return -np.dot(self.ps, self.logps)
 
     def cross_entropy(self, q_dist):
-        assert self.scale_min == q_dist.scale_min, (self.scale_min, q_dist.scale_min)
-        assert self.scale_max == q_dist.scale_max
-        assert self.size == q_dist.size, (self.size, q_dist.size)
+        # Uncommented to support Jax tracing:
+        # assert self.scale_min == q_dist.scale_min, (self.scale_min, q_dist.scale_min)
+        # assert self.scale_max == q_dist.scale_max
+        # assert self.size == q_dist.size, (self.size, q_dist.size)
         return -np.dot(self.ps, q_dist.logps)
 
     def pdf(self, x):
-        return self.ps[self.bins > x][0]
+        return self.ps[np.argmax(self.bins >= x)]
 
     def cdf(self, x):
-        return self.cum_ps[self.bins > x][0]
+        return self.cum_ps[np.argmax(self.bins >= x)]
 
     def ppf(self, q):
-        return np.where(self.cum_ps > q)[0][0] / self.cum_ps.size
+        return np.where(self.cum_ps >= q)[0][0] / self.cum_ps.size
 
     def sample(self):
         raise NotImplementedError
@@ -62,19 +75,21 @@ class HistogramDist(distribution.Distribution):
         num_bins=100,
         verbose=False,
     ):
-        normalized_conditions = [
-            condition.normalize(scale_min, scale_max) for condition in conditions
-        ]
+        normalized_conditions =  [condition.normalize(scale_min, scale_max) for condition in conditions]
 
-        def loss(params):
-            dist = cls.from_params(params, traceable=True)
-            total_loss = sum(
-                condition.loss(dist) for condition in normalized_conditions
-            )
-            return total_loss * 100
+        cond_data = [condition.destructure() for condition in normalized_conditions]
+        if cond_data:
+            cond_classes, cond_params = zip(*cond_data)
+        else:
+            cond_classes, cond_params = [], []
 
-        loss = jit(loss)
-        jac = jit(grad(loss))
+        loss = lambda params: static_loss(  # noqa: E731
+            params, cond_classes, cond_params
+        )
+        jac = lambda params: static_loss_grad(  # noqa: E731
+            params, cond_classes, cond_params
+        )
+
         normalized_dist = cls.from_loss(loss=loss, jac=jac, num_bins=num_bins)
 
         if verbose:
@@ -94,6 +109,34 @@ class HistogramDist(distribution.Distribution):
     def from_params(cls, params, traceable=False):
         logps = nn.log_softmax(params)
         return cls(logps, traceable=traceable)
+
+    def destructure(self):
+        return (
+            HistogramDist,
+            (
+                self.logps,
+                self.ps,
+                self.cum_ps,
+                self.bins,
+                self.size,
+                self.scale_min,
+                self.scale_max,
+            ),
+        )
+
+    @classmethod
+    def structure(cls, params):
+        return cls(
+            direct_init={
+                "logps": params[0],
+                "ps": params[1],
+                "cum_ps": params[2],
+                "bins": params[3],
+                "size": params[4],
+                "scale_min": params[5],
+                "scale_max": params[6],
+            }
+        )
 
     @classmethod
     def from_pairs(cls, pairs):
@@ -119,3 +162,30 @@ class HistogramDist(distribution.Distribution):
     @staticmethod
     def initialize_params(num_bins):
         return onp.full(num_bins, -num_bins)
+
+
+def static_loss(dist_params, cond_classes, cond_params):
+    total_loss = 0.0
+    for (cond_class, cond_param) in zip(cond_classes, cond_params):
+        total_loss += static_condition_loss(dist_params, cond_class, cond_param)
+    return total_loss
+
+
+def static_loss_grad(dist_params, cond_classes, cond_params):
+    total_grad = 0.0
+    for (cond_class, cond_param) in zip(cond_classes, cond_params):
+        total_grad += static_condition_loss_grad(dist_params, cond_class, cond_param)
+    return total_grad
+
+
+@partial(jit, static_argnums=1)
+def static_condition_loss(dist_params, cond_class, cond_param):
+    print(f"Tracing condition loss for {cond_class.__name__} with params {cond_param}")
+    dist = HistogramDist.from_params(dist_params, traceable=True)
+    condition = cond_class.structure(cond_param)
+    return condition.loss(dist) * 100
+
+
+static_condition_loss_grad = jit(
+    grad(static_condition_loss, argnums=0), static_argnums=1
+)
