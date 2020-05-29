3c45dc44847527036907f61dff093664f2c88009
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2020-05-29 00:26:03 -0700

    Apply SmoothnessCondition in log space

--- ergo/distributions/conditions.py
+++ ergo/distributions/conditions.py
@@ -104,10 +104,10 @@ class SmoothnessCondition(Condition):
         window_size = 5
         squared_distance = 0.0
         for i in range(1, window_size + 1):
-            squared_distance += (1 / i) * np.sum(
-                np.square(dist.ps - shift(dist.ps, 1, dist.ps[0]))
+            squared_distance += (1 / i ** 2) * np.sum(
+                np.square(dist.logps - shift(dist.logps, i, dist.logps[0]))
             )
-        return self.weight * np.exp(squared_distance)
+        return self.weight * squared_distance / dist.logps.size
 
     def destructure(self):
         return (SmoothnessCondition, (self.weight,))
