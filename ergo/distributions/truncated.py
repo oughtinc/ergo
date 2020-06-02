40156c07e50e7d61c7ccf6027cf33aa520b5765c
Ben Rachbach <brachbach@gmail.com>
2020-06-02 12:04:05 -0700

    Fix spelling

--- ergo/distributions/truncated.py
+++ ergo/distributions/truncated.py
@@ -19,7 +19,7 @@ def truncate(underlying_dist_class: Distribution, floor: float, ceiling: float):
     class TruncatedDist(Distribution):
         """
         A wrapper around the underlying distribution that throws out
-        probabiliy mass outside the range defined by the floor/ceiling
+        probability mass outside the range defined by the floor/ceiling
         and then renormalizes
 
         :param Distribution: the underlying distribution to truncate
