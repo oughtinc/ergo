bdfe197a46b65fce5e986546346c020f3edc93b9
Ben Rachbach <brachbach@gmail.com>
2020-06-01 21:54:00 -0700

    Finish refactor to reduce confusing code duplication and make truncate more useful. Couldn't figure out how to make it just a class so it's still a function that returns a class.

--- ergo/distributions/truncated.py
+++ ergo/distributions/truncated.py
@@ -6,29 +6,32 @@ from typing import Sequence, Optional
 
 
 def truncate(underlying_dist_class: Distribution, floor: float, ceiling: float):
+    @dataclass
     class TruncatedDist(Distribution):
+        underlying_dist: Distribution
+
         def ppf(self, q):
             raise NotImplementedError
 
         def pdf1(self, x):
-            p_below = self.underlying_dist.cdf(self.floor)
-            p_above = 1 - self.underlying_dist.cdf(self.ceiling)
+            p_below = self.underlying_dist.cdf(floor)
+            p_above = 1 - self.underlying_dist.cdf(ceiling)
             p_inside = 1 - (p_below + p_above)
 
             p_at_x = self.underlying_dist.pdf1(x) * (1 / p_inside)
 
-            return np.where(x < self.floor, 0, np.where(x > self.ceiling, 0, p_at_x))
+            return np.where(x < floor, 0, np.where(x > ceiling, 0, p_at_x))
 
         @classmethod
         def from_params(cls, params):
-            underlying_dist = params.underlying_dist.from_params(params)
-            return cls(underlying_dist, **params)
+            underlying_dist = underlying_dist_class.from_params(params)
+            return cls(underlying_dist)
 
         @classmethod
         def from_conditions(cls, **kwargs):
             underlying_dist = underlying_dist_class.from_conditions(  # type: ignore
                 containing_class=cls, scale_min=floor, scale_max=ceiling, **kwargs
             )
-            return cls(underlying_dist, floor, ceiling)
+            return cls(underlying_dist)
 
     return TruncatedDist
