73e75639e4f45ec12b503a3fd14481e7bb1d1905
Ben Rachbach <brachbach@gmail.com>
2020-06-02 21:23:00 -0700

    Playing around with type. I think maybe this is working

--- ergo/distributions/truncated.py
+++ ergo/distributions/truncated.py
@@ -50,4 +50,4 @@ def truncate(BaseDistClass):
             # expect to encounter xs outside the range.
             return np.where(x < self.floor, 0, np.where(x > self.ceiling, 0, p_at_x))
 
-    return type("TruncatedDist", (BaseDistClass, GenericTruncatedDist), {})
+    return type("TruncatedDist", (GenericTruncatedDist, BaseDistClass), {})
