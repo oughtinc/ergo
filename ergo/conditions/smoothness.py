a94aa6dd37317d92c84196818819868fd3091295
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2020-06-21 22:05:39 -0700

    Updated variable name that changed from HistogramDist to PointDensity

--- ergo/conditions/smoothness.py
+++ ergo/conditions/smoothness.py
@@ -11,9 +11,12 @@ class SmoothnessCondition(condition.Condition):
         squared_distance = 0.0
         for i in range(1, window_size + 1):
             squared_distance += (1 / i ** 2) * np.sum(
-                np.square(dist.normed_log_densities - shift(dist.normed_log_densities, i, dist.normed_log_densities[0]))
+                np.square(
+                    dist.normed_log_densities
+                    - shift(dist.normed_log_densities, i, dist.normed_log_densities[0])
+                )
             )
-        return self.weight * squared_distance / dist.logps.size
+        return self.weight * squared_distance / dist.normed_log_densities.size
 
     def destructure(self):
         return ((SmoothnessCondition,), (self.weight,))
