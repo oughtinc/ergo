3bef26b02a998d049111c81173eb251c34eea8ca
D J Hawthorne <djthorne@users.noreply.github.com>
2020-06-23 14:23:20 +0200

    fix log scale test

--- tests/test_scales.py
+++ tests/test_scales.py
@@ -4,8 +4,9 @@ from ergo.scale import LogScale, Scale, scale_factory
 def test_serialization():
     assert hash(Scale(0, 100)) == hash(Scale(0, 100))
     assert hash(Scale(0, 100)) != hash(Scale(100, 200))
-    assert hash(LogScale(0, 100, 1)) != hash(Scale(0, 100))
-    assert hash(LogScale(0, 100, 10)) != hash(LogScale(0, 100, 10))
+
+    assert hash(LogScale(0, 100, 10)) == hash(LogScale(0, 100, 10))
+    assert hash(LogScale(0, 100, 10)) != hash(LogScale(0, 100, 100))
 
 
 def test_export_import():
