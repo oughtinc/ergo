014bee199fc3290dc1174ae61dbe11dab48d2c30
uvafan <eli.d.lifland@gmail.com>
2020-07-01 00:48:41 -0400

    clean up test_point_density

--- tests/test_point_density.py
+++ tests/test_point_density.py
@@ -2,10 +2,10 @@ import numpy as np
 import pytest
 from scipy.stats import logistic
 
-from ergo.conditions import (  # CrossEntropyCondition,
+from ergo.conditions import (
+    CrossEntropyCondition,
     IntervalCondition,
     MaxEntropyCondition,
-    PointDensityCondition,
 )
 from ergo.distributions.point_density import PointDensity
 from ergo.scale import LogScale, Scale
@@ -15,7 +15,6 @@ from tests.conftest import scales_to_test
 @pytest.mark.parametrize(
     "scale",
     [Scale(0, 5), Scale(-1, 6), Scale(-3, 10), LogScale(0.01, 5, 500)],
-    #    "scale", [LogScale(0.01, 5, 500)],
 )
 @pytest.mark.parametrize(
     "dist_source",
@@ -44,29 +43,23 @@ def test_point_density(scale, dist_source):
     elif dist_source == "denormalized":
         dist = direct_dist.normalize().denormalize(scale)
     elif dist_source == "from_conditions":
-        # cond = CrossEntropyCondition(p_dist=direct_dist)
-        xs, densities = direct_dist.to_lists()
-        cond = PointDensityCondition(xs, densities)
+        cond = CrossEntropyCondition(p_dist=direct_dist)
         dist = PointDensity.from_conditions(
             [cond], fixed_params={"xs": xs}, scale=scale
         )
 
     # PDF
     dist_densities = np.array([float(dist.pdf(x)) for x in xs])
-    # print(f"scale: {scale} dist_source: {dist_source}")
-    # print(f"max pdf diff: {np.max(np.abs(orig_densities-dist_densities))}")
     assert dist_densities == pytest.approx(orig_densities, abs=0.01)
 
     # CDF
     dist_cdfs = np.array([float(dist.cdf(x)) for x in xs])
-    # print(f"max cdf diff: {np.max(np.abs(orig_cdfs-dist_cdfs))}")
     assert dist_cdfs == pytest.approx(orig_cdfs, abs=0.05)
 
     # PPF has low resolution at the low end (because distribution is
     # flat) and at high end (because distribution is flat and log
     # scale is coarse there)
     dist_ppfs = np.array([float(dist.ppf(c)) for c in orig_cdfs[30:60]])
-    # print(f"max ppf diff: {np.max(np.abs(xs[30:60]-dist_ppfs))}")
     assert dist_ppfs == pytest.approx(xs[30:60], abs=0.1)
 
 
