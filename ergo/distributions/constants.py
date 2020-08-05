7de916f1ee3081ee254956ad5fc0923d74a77310
D J Hawthorne <djthorne@users.noreply.github.com>
2020-08-05 22:14:16 +0200

    set default point density grid to 200pts

--- ergo/distributions/constants.py
+++ ergo/distributions/constants.py
@@ -1,7 +1,7 @@
 import jax.numpy as np
 
 # The default number of points PointDensity uses to represent a distribution
-point_density_default_num_points = 400
+point_density_default_num_points = 200
 bin_sizes = np.full(
     point_density_default_num_points, 1 / point_density_default_num_points
 )
