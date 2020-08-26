6919e46b53238e4b5e0db306c17ec405318b8fd8
Eli Lifland <eli.d.lifland@gmail.com>
2020-08-26 14:35:18 -0400

    smooth based on change in slope (second derivative) rather than first derivative to avoid notches

--- ergo/conditions/smoothness.py
+++ ergo/conditions/smoothness.py
@@ -7,16 +7,7 @@ from . import condition
 
 class SmoothnessCondition(condition.Condition):
     def loss(self, dist) -> float:
-        window_size = 5
-        squared_distance = 0.0
-        for i in range(1, window_size + 1):
-            squared_distance += (1 / i ** 2) * np.sum(
-                np.square(
-                    dist.normed_log_densities
-                    - shift(dist.normed_log_densities, i, dist.normed_log_densities[0])
-                )
-            )
-        return self.weight * squared_distance / dist.normed_log_densities.size
+        return self.weight * np.sum(np.square(np.diff(dist.normed_log_densities, n=2)))
 
     def destructure(self):
         return ((SmoothnessCondition,), (self.weight,))
