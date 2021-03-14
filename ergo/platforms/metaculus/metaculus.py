aa7caba412a3b22354fcf326b5521e0c7311962c
Eli Lifland <eli.d.lifland@gmail.com>
2021-03-14 09:57:46 -0400

    Metaculus API call fix

--- ergo/platforms/metaculus/metaculus.py
+++ ergo/platforms/metaculus/metaculus.py
@@ -255,7 +255,7 @@ class Metaculus:
                 return results
 
             r = self.s.get(
-                f"{self.api_url}/questions/?{query_string}&page={current_page}"
+                f"{self.api_url}/questions/?{query_string}&limit=20&offset={20*current_page}"
             )
 
             if r.json() == {"detail": "Invalid page."}:
